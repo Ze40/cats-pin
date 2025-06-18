6874cb019109b60ee8b71061b5904ec561ec9621
Ze40 <maxim.schcerbakov@gmail.com>
2025-06-18 15:18:53 +0300

    style(header): add logo

--- front/src/widgets/header/ui/header.tsx
+++ front/src/widgets/header/ui/header.tsx
@@ -1,11 +1,13 @@
 import { Container } from "@/shared/ui";
 
 import Navigation from "../../navigation/ui/navigation";
+import classes from "./style.module.scss";
 
 const Header = () => {
   return (
     <header className="bg-secondary">
-      <Container>
+      <Container className="flex items-center gap-3xl">
+        <img src="/logo/logo-main.png" alt="logo" className={classes.logo} />
         <Navigation />
       </Container>
     </header>
