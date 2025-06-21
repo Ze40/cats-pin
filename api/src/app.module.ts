15508a7ef06f688fda40605fc8a9185f1fb41c0f
Ze40 <maxim.schcerbakov@gmail.com>
2025-06-21 11:07:15 +0300

    chore(api): create user serv

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { UserModule } from './users/user.module';
 
 @Module({
   imports: [
@@ -7,6 +10,16 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
       envFilePath: '.env',
     }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'cat-pinterest-api-pg',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.DB_USER || 'postgres',
+      password: process.env.DB_PASSWORD || '1',
+      database: process.env.DB_NAME || 'support_lk_db',
+      entities: [],
+    }),
+    UserModule,
   ],
   controllers: [],
   providers: [],
