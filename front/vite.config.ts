0b5e5c6613cf2e064f3ead02e26e14f58822de97
Ze40 <maxim.schcerbakov@gmail.com>
2025-06-21 11:34:32 +0300

    chore(docker): back to init config

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,11 +6,4 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
-  server: {
-    host: true,
-    port: 5173,
-    watch: {
-      usePolling: true,
-    },
-  },
 });
