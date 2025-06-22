68bbd2f9a366f80a8297eacf35be0aae8926595c
Ze40 <maxim.schcerbakov@gmail.com>
2025-06-22 16:27:07 +0300

    refactor(all-cats): refactor with react query

--- front/src/pages/content-pages/all-cats/all-cats.tsx
+++ front/src/pages/content-pages/all-cats/all-cats.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 
 import { api } from "@/api/api";
 import { Cat } from "@/entities";
@@ -8,30 +8,26 @@ import { Container } from "@/shared/ui";
 import ContentPageLayout from "../layout";
 
 const AllCatsPage = () => {
-  const [cats, setCats] = useState<Cat[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const {
+    data: cats = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["cats"],
+    queryFn: () =>
+      api
+        .get<Cat[]>("/cats", {
+          headers: {
+            "content-page": 1,
+          },
+        })
+        .then((res) => res.data),
+  });
 
-  useEffect(() => {
-    setIsLoading(true);
-    api
-      .get<Cat[]>("/cats", {
-        headers: {
-          "content-page": 1,
-        },
-      })
-      .then((response) => {
-        setCats(response.data);
-        setIsLoading(false);
-      })
-      .catch((error) => {
-        console.error("Ошибка при получении котов:", error);
-      });
-  }, []);
-
-  useEffect(() => {
-    console.log(cats);
-  }, [cats]);
-
+  if (isError) {
+    console.error("Ошибка при получении котов");
+    return <div>Произошла ошибка</div>;
+  }
   return (
     <ContentPageLayout>
       <Container>
