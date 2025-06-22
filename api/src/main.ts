d28efca70fba3a3878d393f25338ee53d9ecbb3d
Ze40 <maxim.schcerbakov@gmail.com>
2025-06-22 13:54:29 +0300

    chore(api): enabled cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -4,6 +4,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === 'development') {
+    app.enableCors();
+  } else {
+    app.enableCors({
+      origin: 'https://production-domain.com',
+    });
+  }
   await app.listen(3333);
 }
 bootstrap();
