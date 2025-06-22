3878d45482230f6a99ad450a40d0e20a1d0641ee
Ze40 <maxim.schcerbakov@gmail.com>
2025-06-22 16:01:30 +0300

    feat(front): get fav cats from server

--- front/src/pages/content-pages/favorite/favorite.tsx
+++ front/src/pages/content-pages/favorite/favorite.tsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
+
+import { api } from "@/api/api";
+import { Cat } from "@/entities";
+import { TheCardViewer } from "@/feat";
 import { Container } from "@/shared/ui";
 
 import ContentPageLayout from "../layout";
 
 const FavoritePage = () => {
+  const [cats, setCats] = useState<Cat[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    api
+      .get<Cat[]>("/cats/likes")
+      .then((response) => {
+        setCats(response.data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Ошибка при получении котов:", error);
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(cats);
+  }, [cats]);
+
   return (
     <ContentPageLayout>
       <Container>
-        <></>
+        <TheCardViewer items={cats} isLoading={isLoading} />
       </Container>
     </ContentPageLayout>
   );
